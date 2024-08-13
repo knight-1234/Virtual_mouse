af51d2a921d0d74640e7ded98d5b8a18af558436
Romeo <95969929+knight-1234@users.noreply.github.com>
2024-08-13 09:57:55 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def angle(a, b, c):
+def get_angle(a, b, c):
     radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
     angle = np.abs(np.degrees(radians))
     return angle
 
 
-def distance(landmark_list):
+def get_distance(landmark_list):
     if len(landmark_list) < 2:
         return
 
